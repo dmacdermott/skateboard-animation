fdb9ba92ed581cd2a0f77312c689e6038b0b5400
dmacdermott <damianmacdermott@gmail.com>
2021-02-22 23:58:08 +0900

    progress made on animations

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const engine = new BABYLON.Engine(canvas, true);
 
 const createScene = (trick) => {
   const scene = new BABYLON.Scene(engine);
+  scene.clearColor = new BABYLON.Color3.FromHexString("#00bbf9");
 
   const camera = new BABYLON.ArcRotateCamera(
     "camera",
@@ -15,6 +16,7 @@ const createScene = (trick) => {
   );
 
   camera.attachControl(canvas, true);
+
   const light = new BABYLON.HemisphericLight(
     "light",
     new BABYLON.Vector3(0, 1, 0),
@@ -28,6 +30,7 @@ const createScene = (trick) => {
   ).then(() => {
     const skateboard = scene.getMeshByName("__root__");
     skateboard.position.y = 0.5;
+    skateboard.position.x = 3.5;
     skateboard.rotationQuaternion = null;
     const frameRate = 10;
 
@@ -283,17 +286,166 @@ const createScene = (trick) => {
     scene.beginAnimation(wheelRF, 0, 30, true);
     scene.beginAnimation(wheelLF, 0, 30, true);
     ollieAnimationGroup.play(false);
-    kickflipAnimationGroup.play(true);
+
+    let currentAnimation = ollieAnimationGroup;
+
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI(
+      "UI"
+    );
+
+    var selectBox = new BABYLON.GUI.SelectionPanel("sp");
+    selectBox.width = 0.25;
+    selectBox.height = 0.48;
+    selectBox.background = "#FFF";
+    selectBox.headerColor = "#9b5de5";
+    selectBox.buttonColor = "#9b5de5";
+    selectBox.buttonBackground = "#FFF";
+
+    selectBox.horizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    selectBox.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+
+    advancedTexture.addControl(selectBox);
+
+    var trickGroup = new BABYLON.GUI.RadioGroup("Tricks");
+    trickGroup.background = "red";
+    trickGroup.addRadio(
+      "Ollie",
+      function () {
+        currentAnimation.reset();
+        currentAnimation.stop();
+        currentAnimation = ollieAnimationGroup;
+        ollieAnimationGroup.play(true);
+      },
+      true
+    );
+    trickGroup.addRadio("Kickflip", function () {
+      currentAnimation.reset();
+
+      currentAnimation.stop();
+      currentAnimation = kickflipAnimationGroup;
+      kickflipAnimationGroup.play(true);
+    });
+
+    //speed Slider
+    // var rotateGroup = new BABYLON.GUI.SliderGroup("Rotation", "S");
+    // rotateGroup.addSlider(
+    //   "Angle",
+    //   orientateY,
+    //   "degs",
+    //   0,
+    //   2 * Math.PI,
+    //   0,
+    //   displayValue
+    // );
+
+    selectBox.addGroup(trickGroup);
+    selectBox.addGroup(speedGroup);
   });
 
   //GROUND
-  const groundMat = new BABYLON.StandardMaterial("groundMat");
-  groundMat.diffuseColor = new BABYLON.Color3(0.13, 0.13, 0.13);
-  const ground = BABYLON.MeshBuilder.CreateGround("ground", {
-    width: 20,
-    height: 20,
+  // const groundMat = new BABYLON.StandardMaterial("groundMat");
+  // groundMat.diffuseColor = new BABYLON.Color3.FromHexString("#3d5a80");
+  // const ground = BABYLON.MeshBuilder.CreateGround("ground", {
+  //   width: 20,
+  //   height: 20,
+  // });
+  // ground.material = groundMat;
+
+  BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "./assets/road/",
+    "CUPIC_ROAD.gltf"
+  ).then(() => {
+    const road = scene.getMeshByName("node-0");
+    road.position.y = -1;
+    road.scaling = new BABYLON.Vector3(0.1, 0.1, 0.075);
   });
-  ground.material = groundMat;
+
+  BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "./assets/trashcan/",
+    "TrashCan_517.gltf"
+  ).then(() => {
+    const trashCan = scene.meshes[19];
+    console.log(scene.meshes);
+    trashCan.position.y = -1;
+    trashCan.position.x = 3.5;
+    trashCan.scaling = new BABYLON.Vector3(0.1, 0.14, 0.1);
+
+    const trashAnimation = new BABYLON.Animation(
+      "trashCanAnimation",
+      "position.z",
+      60,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+    const trashKeys = [];
+    trashKeys.push({
+      frame: 0,
+      value: -33,
+    });
+    trashKeys.push({
+      frame: 150,
+      value: 33,
+    });
+
+    trashAnimation.setKeys(trashKeys);
+    trashCan.animations = [];
+    trashCan.animations.push(trashAnimation);
+    scene.beginAnimation(trashCan, 0, 150, true);
+  });
+
+  BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "./assets/stopsign/",
+    "1358 Stop Sign.gltf"
+  ).then(() => {
+    const stopSign = scene.meshes[21];
+    console.log(scene.meshes);
+    stopSign.rotationQuaternion = null;
+    stopSign.position.y = -1;
+    stopSign.position.x = 10;
+    stopSign.rotation.y = -1.6;
+    stopSign.scaling = new BABYLON.Vector3(0.075, 0.075, 0.075);
+
+    scene.registerBeforeRender(function () {
+      stopSign.position.z += 0.5;
+
+      if (stopSign.position.z < 33 && stopSign.position.z > -33) {
+        stopSign.visibility = 1;
+      } else {
+        stopSign.visibility = 0;
+      }
+      if (stopSign.position.z === 300) {
+        stopSign.position.z = -40;
+      }
+    });
+
+    // const stopSignAnimation = new BABYLON.Animation(
+    //   "stopSignAnimation",
+    //   "position.z",
+    //   60,
+    //   BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    //   BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE
+    // );
+    const stopSignKeys = [];
+    stopSignKeys.push({
+      frame: 0,
+      value: -33,
+    });
+    stopSignKeys.push({
+      frame: 150,
+      value: 33,
+    });
+
+    stopSignAnimation.setKeys(stopSignKeys);
+    stopSign.animations = [];
+    stopSign.animations.push(stopSignAnimation);
+
+    scene.beginAnimation(stopSign, 0, 250, true);
+  });
+
   return scene;
 };
 
