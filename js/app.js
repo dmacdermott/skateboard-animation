5e26ce0283bff3da309e6ff02c53bea48d80cfc2
dmacdermott <damianmacdermott@gmail.com>
2021-02-23 13:49:13 +0900

    commit to set up gh pages

--- js/app.js
+++ js/app.js
@@ -30,7 +30,6 @@ const createScene = (trick) => {
   ).then(() => {
     const skateboard = scene.getMeshByName("__root__");
     skateboard.position.y = 0.5;
-    skateboard.position.x = 3.5;
     skateboard.rotationQuaternion = null;
     const frameRate = 10;
 
@@ -115,7 +114,6 @@ const createScene = (trick) => {
     ollieAnimationGroup.addTargetedAnimation(rotX, skateboard);
 
     //KICKFLIP
-
     posY = new BABYLON.Animation(
       "posY",
       "position.y",
@@ -236,6 +234,167 @@ const createScene = (trick) => {
     kickflipAnimationGroup.addTargetedAnimation(rotX, skateboard);
     kickflipAnimationGroup.addTargetedAnimation(posY, skateboard);
 
+    //360 Flip
+    posY = new BABYLON.Animation(
+      "posY",
+      "position.y",
+      10,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+    keyFramesPy = [];
+
+    keyFramesPy.push({
+      frame: 0,
+      value: 0,
+    });
+    keyFramesPy.push({
+      frame: 1,
+      value: 2.5,
+    });
+
+    keyFramesPy.push({
+      frame: 5,
+      value: 4,
+    });
+    keyFramesPy.push({
+      frame: 7,
+      value: 4,
+    });
+
+    keyFramesPy.push({
+      frame: frameRate,
+      value: 0.5,
+    });
+    keyFramesPy.push({
+      frame: frameRate + 5,
+      value: 0.5,
+    });
+
+    posY.setKeys(keyFramesPy);
+
+    //Rotation Animation
+    rotX = new BABYLON.Animation(
+      "rotX",
+      "rotation.x",
+      frameRate,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+
+    keyFramesRx = [];
+
+    keyFramesRx.push({
+      frame: 0,
+      value: 0,
+    });
+
+    keyFramesRx.push({
+      frame: 2,
+      value: 1,
+    });
+
+    keyFramesRx.push({
+      frame: 5,
+      value: -0.4,
+    });
+
+    keyFramesRx.push({
+      frame: frameRate,
+      value: 0,
+    });
+    keyFramesRx.push({
+      frame: frameRate + 5,
+      value: 0,
+    });
+
+    rotX.setKeys(keyFramesRx);
+    //Rotation Animation
+    var rotZ = new BABYLON.Animation(
+      "rotZ",
+      "rotation.z",
+      frameRate,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+
+    keyFramesRz = [];
+
+    keyFramesRz.push({
+      frame: 0,
+      value: 0,
+    });
+    keyFramesRz.push({
+      frame: 1,
+      value: 0,
+    });
+
+    keyFramesRz.push({
+      frame: 3,
+      value: 1,
+    });
+
+    keyFramesRz.push({
+      frame: 6,
+      value: 6.3,
+    });
+
+    keyFramesRz.push({
+      frame: frameRate,
+      value: 6.3,
+    });
+    keyFramesRz.push({
+      frame: frameRate + 5,
+      value: 6.3,
+    });
+
+    rotZ.setKeys(keyFramesRz);
+    var rotY = new BABYLON.Animation(
+      "rotY",
+      "rotation.y",
+      frameRate,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+
+    keyFramesRy = [];
+
+    keyFramesRy.push({
+      frame: 0,
+      value: 0,
+    });
+    keyFramesRy.push({
+      frame: 1,
+      value: 0,
+    });
+
+    keyFramesRy.push({
+      frame: 3,
+      value: -3,
+    });
+
+    keyFramesRy.push({
+      frame: 6,
+      value: -6.3,
+    });
+
+    keyFramesRy.push({
+      frame: frameRate,
+      value: -6.3,
+    });
+    keyFramesRy.push({
+      frame: frameRate + 5,
+      value: -6.3,
+    });
+
+    rotY.setKeys(keyFramesRy);
+
+    const treFlipAnimationGroup = new BABYLON.AnimationGroup("360 Flip");
+    treFlipAnimationGroup.addTargetedAnimation(rotZ, skateboard);
+    treFlipAnimationGroup.addTargetedAnimation(rotX, skateboard);
+    treFlipAnimationGroup.addTargetedAnimation(rotY, skateboard);
+    treFlipAnimationGroup.addTargetedAnimation(posY, skateboard);
+
     // WHEEL MOVEMENT
     const wheelRB = scene.getMeshByName("wheel back right_0");
     const wheelLB = scene.getMeshByName("wheel back left_0");
@@ -308,7 +467,6 @@ const createScene = (trick) => {
     advancedTexture.addControl(selectBox);
 
     var trickGroup = new BABYLON.GUI.RadioGroup("Tricks");
-    trickGroup.background = "red";
     trickGroup.addRadio(
       "Ollie",
       function () {
@@ -325,6 +483,13 @@ const createScene = (trick) => {
       currentAnimation.stop();
       currentAnimation = kickflipAnimationGroup;
       kickflipAnimationGroup.play(true);
+    });
+    trickGroup.addRadio("360 Flip", function () {
+      currentAnimation.reset();
+
+      currentAnimation.stop();
+      currentAnimation = treFlipAnimationGroup;
+      treFlipAnimationGroup.play(true);
     });
 
     //speed Slider
@@ -344,107 +509,13 @@ const createScene = (trick) => {
   });
 
   //GROUND
-  // const groundMat = new BABYLON.StandardMaterial("groundMat");
-  // groundMat.diffuseColor = new BABYLON.Color3.FromHexString("#3d5a80");
-  // const ground = BABYLON.MeshBuilder.CreateGround("ground", {
-  //   width: 20,
-  //   height: 20,
-  // });
-  // ground.material = groundMat;
-
-  BABYLON.SceneLoader.ImportMeshAsync(
-    "",
-    "./assets/road/",
-    "CUPIC_ROAD.gltf"
-  ).then(() => {
-    const road = scene.getMeshByName("node-0");
-    road.position.y = -1;
-    road.scaling = new BABYLON.Vector3(0.1, 0.1, 0.075);
+  const groundMat = new BABYLON.StandardMaterial("groundMat");
+  groundMat.diffuseColor = new BABYLON.Color3.FromHexString("#3d5a80");
+  const ground = BABYLON.MeshBuilder.CreateGround("ground", {
+    width: 20,
+    height: 20,
   });
-
-  BABYLON.SceneLoader.ImportMeshAsync(
-    "",
-    "./assets/trashcan/",
-    "TrashCan_517.gltf"
-  ).then(() => {
-    const trashCan = scene.meshes[19];
-    console.log(scene.meshes);
-    trashCan.position.y = -1;
-    trashCan.position.x = 3.5;
-    trashCan.scaling = new BABYLON.Vector3(0.1, 0.14, 0.1);
-
-    const trashAnimation = new BABYLON.Animation(
-      "trashCanAnimation",
-      "position.z",
-      60,
-      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-    );
-    const trashKeys = [];
-    trashKeys.push({
-      frame: 0,
-      value: -33,
-    });
-    trashKeys.push({
-      frame: 150,
-      value: 33,
-    });
-
-    trashAnimation.setKeys(trashKeys);
-    trashCan.animations = [];
-    trashCan.animations.push(trashAnimation);
-    scene.beginAnimation(trashCan, 0, 150, true);
-  });
-
-  BABYLON.SceneLoader.ImportMeshAsync(
-    "",
-    "./assets/stopsign/",
-    "1358 Stop Sign.gltf"
-  ).then(() => {
-    const stopSign = scene.meshes[21];
-    console.log(scene.meshes);
-    stopSign.rotationQuaternion = null;
-    stopSign.position.y = -1;
-    stopSign.position.x = 10;
-    stopSign.rotation.y = -1.6;
-    stopSign.scaling = new BABYLON.Vector3(0.075, 0.075, 0.075);
-
-    scene.registerBeforeRender(function () {
-      stopSign.position.z += 0.5;
-
-      if (stopSign.position.z < 33 && stopSign.position.z > -33) {
-        stopSign.visibility = 1;
-      } else {
-        stopSign.visibility = 0;
-      }
-      if (stopSign.position.z === 300) {
-        stopSign.position.z = -40;
-      }
-    });
-
-    // const stopSignAnimation = new BABYLON.Animation(
-    //   "stopSignAnimation",
-    //   "position.z",
-    //   60,
-    //   BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-    //   BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE
-    // );
-    const stopSignKeys = [];
-    stopSignKeys.push({
-      frame: 0,
-      value: -33,
-    });
-    stopSignKeys.push({
-      frame: 150,
-      value: 33,
-    });
-
-    stopSignAnimation.setKeys(stopSignKeys);
-    stopSign.animations = [];
-    stopSign.animations.push(stopSignAnimation);
-
-    scene.beginAnimation(stopSign, 0, 250, true);
-  });
+  ground.material = groundMat;
 
   return scene;
 };
